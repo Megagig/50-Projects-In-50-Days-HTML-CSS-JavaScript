cdfb8ee7d209fa39d4e34cf6cba0d4e4e33b4a15
ANTHONY OBI <Megagigsoftwaresolutions@gmail.com>
2023-05-04 11:25:09 +0100

    Add the javascript logic

---
+++ Todo List App/script.js
@@ -0,0 +1,58 @@
+// select the DOM elements
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todoUl = document.getElementById('todos');
+
+//Add event listener to the form
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addTodo();
+});
+
+//Add todo function
+function addTodo(todo) {
+  let todoText = input.value;
+  if (todo) {
+    todoText = todo.text;
+  }
+  if (todoText) {
+    const todoEl = document.createElement('li');
+    if (todo && todo.completed) {
+      todoEl.classList.add('completed');
+    }
+    todoEl.innerText = todoText;
+    todoEl.addEventListener('click', () => {
+      todoEl.classList.toggle('completed');
+      updateLS();
+    });
+    todoEl.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      todoEl.remove();
+      updateLS();
+    });
+    todoUl.appendChild(todoEl);
+    input.value = '';
+    updateLS();
+  }
+}
+
+//Update local storage
+function updateLS() {
+  const todosEl = document.querySelectorAll('li');
+  const todos = [];
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains('completed'),
+    });
+  });
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+//Get todos from local storage  and display them
+const todos = JSON.parse(localStorage.getItem('todos'));
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
